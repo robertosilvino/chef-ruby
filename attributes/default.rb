a14665cd9833246cddd88a276b001931a178cce7
Gabriel Mazetto <brodock@gmail.com>
2014-12-04 16:11:09 -0200

    Version 0.3.2
    Updated ruby-installer metadata to fix checksum

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,7 +5,7 @@ default['ruby']['flavor']       = 'ruby'
 
 # Override ruby install attributes to get latest version
 set['ruby_install']['version'] = '0.5.0'
-set['ruby_install']['version'] = 'aa4448c2c356510cc7c2505961961a17bd3f3435842831e04c8516eb703afd19'
+set['ruby_install']['checksum'] = 'aa4448c2c356510cc7c2505961961a17bd3f3435842831e04c8516eb703afd19'
 
 case node['platform_family']
 when 'debian', 'ubuntu'
